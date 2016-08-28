e705a27a7959060bddc2252f038d91d12ab85407
Yasser shaikh <yasser.s@directi.com>
2016-08-28 14:03:13 +0530

    more fixes

--- js/app/main-controller.js
+++ js/app/main-controller.js
@@ -1,18 +1,51 @@
 angular.module('timezoneApp').controller('mainController', [
     "$scope", "storageService", "searchService", function ($scope, storageService, searchService) {
+
+    constructor  = function(){
+        storageService.get()
+            .then(function(data){
+                var previouslyAddedTimezones = data;
+                console.log("previouslyAddedTimezones", data);
+
+                $scope.selectedTimezones = [];
+                var utcDate = moment().utc();
+                var utcDisplay = moment().utc().format('h:mm a');
+                $scope.utc = { Id: "utc", Abbreviation: "UTC", Name: "Universal Time Coordinated", Offset: 0, Date: utcDate, Display:  utcDisplay};
+                $scope.selectedTimezones.push($scope.utc);
+                if(previouslyAddedTimezones){
+                    _.each(previouslyAddedTimezones, function(item){
+                        if(item.Id){
+                            $scope.selectedTimezones.push(item);
+                        }
+                    });
+                    
+                }
+                $scope.selected = null;
+            });
+    },
+
     $scope.storage = storageService;
     $scope.$watch('storage.data', function() {
         $scope.timezoneList = $scope.storage.data;
+    });
+    $scope.$watch('selected', function(newValue, oldValue) {
+        if(newValue && newValue["Abbreviation"]){
+            newValue.Date = moment($scope.utc.Date).add('hours', newValue.Offset);
+            newValue.Display = moment(newValue.Date).format('h:mm a');
+            storageService.set(newValue);
+            $scope.selectedTimezones.push(newValue);
+            $scope.selected = null;
+        }
     });
     $scope.storage.findAll(function(data){
         $scope.timezoneList = data;
         $scope.$apply();
     });
-    $scope.add = function() {
-        storageService.add($scope.newContent);
-        $scope.newContent = '';
-    }
     $scope.remove = function(timezone) {
+        $scope.selectedTimezones = _.reject($scope.selectedTimezones, function(item) {
+            console.log(timezone.Id, item.Id);
+            return item.Id === timezone.Id;
+        });
         storageService.remove(timezone);
     }
     $scope.removeAll = function() {
@@ -20,14 +53,10 @@ angular.module('timezoneApp').controller('mainController', [
     }
     $scope.toggleCompleted = function() {
         storageService.sync();
-    }  
-
-    $scope.getTimeZone = function(searchTerm) {
-        var a = searchService.dataSearch(searchTerm);
-        console.log(a);
-        return a; 
     }
-  
-    $scope.statesWithFlags = [{'name':'Alabama','flag':'5/5c/Flag_of_Alabama.svg/45px-Flag_of_Alabama.svg.png'},{'name':'Alaska','flag':'e/e6/Flag_of_Alaska.svg/43px-Flag_of_Alaska.svg.png'},{'name':'Arizona','flag':'9/9d/Flag_of_Arizona.svg/45px-Flag_of_Arizona.svg.png'},{'name':'Arkansas','flag':'9/9d/Flag_of_Arkansas.svg/45px-Flag_of_Arkansas.svg.png'},{'name':'California','flag':'0/01/Flag_of_California.svg/45px-Flag_of_California.svg.png'},{'name':'Colorado','flag':'4/46/Flag_of_Colorado.svg/45px-Flag_of_Colorado.svg.png'},{'name':'Connecticut','flag':'9/96/Flag_of_Connecticut.svg/39px-Flag_of_Connecticut.svg.png'},{'name':'Delaware','flag':'c/c6/Flag_of_Delaware.svg/45px-Flag_of_Delaware.svg.png'},{'name':'Florida','flag':'f/f7/Flag_of_Florida.svg/45px-Flag_of_Florida.svg.png'},{'name':'Georgia','flag':'5/54/Flag_of_Georgia_%28U.S._state%29.svg/46px-Flag_of_Georgia_%28U.S._state%29.svg.png'},{'name':'Hawaii','flag':'e/ef/Flag_of_Hawaii.svg/46px-Flag_of_Hawaii.svg.png'},{'name':'Idaho','flag':'a/a4/Flag_of_Idaho.svg/38px-Flag_of_Idaho.svg.png'},{'name':'Illinois','flag':'0/01/Flag_of_Illinois.svg/46px-Flag_of_Illinois.svg.png'},{'name':'Indiana','flag':'a/ac/Flag_of_Indiana.svg/45px-Flag_of_Indiana.svg.png'},{'name':'Iowa','flag':'a/aa/Flag_of_Iowa.svg/44px-Flag_of_Iowa.svg.png'},{'name':'Kansas','flag':'d/da/Flag_of_Kansas.svg/46px-Flag_of_Kansas.svg.png'},{'name':'Kentucky','flag':'8/8d/Flag_of_Kentucky.svg/46px-Flag_of_Kentucky.svg.png'},{'name':'Louisiana','flag':'e/e0/Flag_of_Louisiana.svg/46px-Flag_of_Louisiana.svg.png'},{'name':'Maine','flag':'3/35/Flag_of_Maine.svg/45px-Flag_of_Maine.svg.png'},{'name':'Maryland','flag':'a/a0/Flag_of_Maryland.svg/45px-Flag_of_Maryland.svg.png'},{'name':'Massachusetts','flag':'f/f2/Flag_of_Massachusetts.svg/46px-Flag_of_Massachusetts.svg.png'},{'name':'Michigan','flag':'b/b5/Flag_of_Michigan.svg/45px-Flag_of_Michigan.svg.png'},{'name':'Minnesota','flag':'b/b9/Flag_of_Minnesota.svg/46px-Flag_of_Minnesota.svg.png'},{'name':'Mississippi','flag':'4/42/Flag_of_Mississippi.svg/45px-Flag_of_Mississippi.svg.png'},{'name':'Missouri','flag':'5/5a/Flag_of_Missouri.svg/46px-Flag_of_Missouri.svg.png'},{'name':'Montana','flag':'c/cb/Flag_of_Montana.svg/45px-Flag_of_Montana.svg.png'},{'name':'Nebraska','flag':'4/4d/Flag_of_Nebraska.svg/46px-Flag_of_Nebraska.svg.png'},{'name':'Nevada','flag':'f/f1/Flag_of_Nevada.svg/45px-Flag_of_Nevada.svg.png'},{'name':'New Hampshire','flag':'2/28/Flag_of_New_Hampshire.svg/45px-Flag_of_New_Hampshire.svg.png'},{'name':'New Jersey','flag':'9/92/Flag_of_New_Jersey.svg/45px-Flag_of_New_Jersey.svg.png'},{'name':'New Mexico','flag':'c/c3/Flag_of_New_Mexico.svg/45px-Flag_of_New_Mexico.svg.png'},{'name':'New York','flag':'1/1a/Flag_of_New_York.svg/46px-Flag_of_New_York.svg.png'},{'name':'North Carolina','flag':'b/bb/Flag_of_North_Carolina.svg/45px-Flag_of_North_Carolina.svg.png'},{'name':'North Dakota','flag':'e/ee/Flag_of_North_Dakota.svg/38px-Flag_of_North_Dakota.svg.png'},{'name':'Ohio','flag':'4/4c/Flag_of_Ohio.svg/46px-Flag_of_Ohio.svg.png'},{'name':'Oklahoma','flag':'6/6e/Flag_of_Oklahoma.svg/45px-Flag_of_Oklahoma.svg.png'},{'name':'Oregon','flag':'b/b9/Flag_of_Oregon.svg/46px-Flag_of_Oregon.svg.png'},{'name':'Pennsylvania','flag':'f/f7/Flag_of_Pennsylvania.svg/45px-Flag_of_Pennsylvania.svg.png'},{'name':'Rhode Island','flag':'f/f3/Flag_of_Rhode_Island.svg/32px-Flag_of_Rhode_Island.svg.png'},{'name':'South Carolina','flag':'6/69/Flag_of_South_Carolina.svg/45px-Flag_of_South_Carolina.svg.png'},{'name':'South Dakota','flag':'1/1a/Flag_of_South_Dakota.svg/46px-Flag_of_South_Dakota.svg.png'},{'name':'Tennessee','flag':'9/9e/Flag_of_Tennessee.svg/46px-Flag_of_Tennessee.svg.png'},{'name':'Texas','flag':'f/f7/Flag_of_Texas.svg/45px-Flag_of_Texas.svg.png'},{'name':'Utah','flag':'f/f6/Flag_of_Utah.svg/45px-Flag_of_Utah.svg.png'},{'name':'Vermont','flag':'4/49/Flag_of_Vermont.svg/46px-Flag_of_Vermont.svg.png'},{'name':'Virginia','flag':'4/47/Flag_of_Virginia.svg/44px-Flag_of_Virginia.svg.png'},{'name':'Washington','flag':'5/54/Flag_of_Washington.svg/46px-Flag_of_Washington.svg.png'},{'name':'West Virginia','flag':'2/22/Flag_of_West_Virginia.svg/46px-Flag_of_West_Virginia.svg.png'},{'name':'Wisconsin','flag':'2/22/Flag_of_Wisconsin.svg/45px-Flag_of_Wisconsin.svg.png'},{'name':'Wyoming','flag':'b/bc/Flag_of_Wyoming.svg/43px-Flag_of_Wyoming.svg.png'}];
+    $scope.getTimeZone = function(searchTerm) {
+        return searchService.dataSearch(searchTerm);
+    }
 
+    constructor();
 }]);
--- js/app/storage-service.js
+++ js/app/storage-service.js
@@ -1,4 +1,4 @@
-angular.module('timezoneApp').service('storageService', function () {
+angular.module('timezoneApp').service('storageService', function ($q) {
     var _this = this;
     this.data = [];
     this.findAll = function(callback) {
@@ -14,25 +14,51 @@ angular.module('timezoneApp').service('storageService', function () {
         });
     }
     this.sync = function() {
-        chrome.storage.sync.set({timeZone: this.data}, function() {
-            console.log('Data is stored in Chrome storage');
+        chrome.storage.sync.set({'timezone': this.data}, function() {
+            console.log('syn callback');
         });
     }
-    this.add = function (newtimezone) {
-        var id = this.data.length + 1;
-        var timeZone = {
-            id: id,
-            zone: newtimezone
-        };
-        this.data.push(timeZone);
+    this.add = function (item) {
+        this.data.push(item);
         this.sync();
     }
-    this.remove = function(timeZone) {
-        this.data.splice(this.data.indexOf(timeZone), 1);
+    this.remove = function(item) {
+        this.data.splice(this.data.indexOf(item), 1);
         this.sync();
     }
     this.removeAll = function() {
         this.data = [];
         this.sync();
+    },
+    this.set = function(value) {
+        var key = 'timezone';
+        this.get(key)
+            .then(function(data){
+                var finalData = [];
+                console.log(data.length);
+                if(data.length){
+                    finalData = data;
+                }
+                else{
+                    finalData.push(data);
+                }
+                finalData.push(value);
+                var deferred = $q.defer();
+                var data = {};
+                data[key] = value;
+                chrome.storage.local.set(data, function() {
+                    deferred.resolve({});
+                });
+                return deferred.promise;
+            });
+        
+    },
+    this.get = function() {
+        var key = 'timezone';
+        var deferred = $q.defer();
+        chrome.storage.local.get(key, function(data) {
+            deferred.resolve(data);
+        });
+        return deferred.promise;
     }
 });
