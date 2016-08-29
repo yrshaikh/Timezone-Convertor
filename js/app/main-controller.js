c8018873966e512a36eeb28c8e0b7b18f03f4ac0
Ali Rizvi <ali.ri@media.net>
2016-08-30 01:06:40 +0530

    am-pm switch working, changes refleted after time change

--- js/app/main-controller.js
+++ js/app/main-controller.js
@@ -13,6 +13,8 @@ angular.module('timezoneApp').controller('mainController', [
                 if(previouslyAddedTimezones){
                     _.each(previouslyAddedTimezones, function(item){
                         if(item.Id){
+                            item.Date = moment().utc().add('hours', item.Offset).format('MMMM Do YYYY, h:mm:ss a');
+                            item.Display = moment().utc().add('hours', item.Offset).format('h:mm A');
                             $scope.selectedTimezones.push(item);
                         }
                     });                    
@@ -24,6 +26,9 @@ angular.module('timezoneApp').controller('mainController', [
     $scope.storage = storageService;
     $scope.$watch('storage.data', function() {
         $scope.timezoneList = $scope.storage.data;
+    });
+    $scope.$watch('timeformat', function() {
+        timeFormatChanged();
     });
     $scope.$watch('selected', function(newValue, oldValue) {
         if(newValue && newValue["Abbreviation"]){
@@ -56,7 +61,38 @@ angular.module('timezoneApp').controller('mainController', [
         }
     }
     $scope.save = function(timezone){
-        timezone.edit = false;         
+        timezone.edit = false;
+        timeChanged(timezone);         
     }
     constructor();
+
+    var timeChanged = function(timezone){
+        var hour = timezone.editObject.hour - moment(timezone.Date, 'MMMM Do YYYY, h:mm:ss a').format('h');
+        var minute = timezone.editObject.minute - moment(timezone.Date, 'MMMM Do YYYY, h:mm:ss a').format('mm');
+        var timeformat = timezone.editObject.a;
+        //todo code for timezone change
+/*        if(moment(timezone.Date, 'MMMM Do YYYY, h:mm:ss a').format('a') === timezone)
+            hour+=12;*/
+        
+
+        $scope.selectedTimezones[0].Date = moment($scope.selectedTimezones[0].Date, 'MMMM Do YYYY, h:mm:ss a').add('hours', hour).add('minutes', minute).format('MMMM Do YYYY, h:mm:ss a');
+        $scope.selectedTimezones[0].Display = moment($scope.selectedTimezones[0].Date, 'MMMM Do YYYY, h:mm:ss a').format('h:mm A')
+        _.each($scope.selectedTimezones, function(item, index){
+            if(index !== 0){
+                item.Date = moment($scope.selectedTimezones[0].Date, 'MMMM Do YYYY, h:mm:ss a').add('hours', item.Offset).format('MMMM Do YYYY, h:mm:ss a');
+                item.Display = moment(item.Date, 'MMMM Do YYYY, h:mm:ss a').format('h:mm A');
+            }
+        })
+    };
+
+    var timeFormatChanged = function(){
+        if($scope.timeformat === 'ampm')
+            var newTimeFormat = 'h:mm A';
+        else
+            var newTimeFormat = 'H:mm';
+
+        _.each($scope.selectedTimezones, function(item, index){
+            item.Display = moment(item.Date, 'MMMM Do YYYY, h:mm:ss a').format(newTimeFormat);
+        })
+    }
 }]);
